85c1f86ebae5b18a6b6a9f742b458f157ebf4c47
henryclyde <hen.clyde@gmail.com>
2024-03-05 00:07:10 -0700

    Created project and modified title on home page

---
+++ MovingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
